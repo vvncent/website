24f794d0fe1aaa7c235b068fe13e474118599d72
Vincent Lin <vincent2@alumni.cmu.edu>
2025-10-26 03:19:23 -0700

    Added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="icons/favicon.png" type="image/png" />
+      </head>
       <body>{children}</body>
     </html>
   )
